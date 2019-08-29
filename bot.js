d4e142d4888b25d08d5219971b2193ad113deac5
ActualStraw <54654636+ActualStraw@users.noreply.github.com>
2019-08-29 01:21:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NjE2NDI5Mzc4NDc3NDkwMTc2.XWdROg.wa1jW_ngv3qrX74tCpmamnhbJys');
+
+client.on('message', message => {
+	if (message.content === 'ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-
-  	if (message.content === 'hi') {
-  		message.reply('i am fine, You ?');
-  	}
+	if (message.content === 'who are you?') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('I am Straw girl version :wink:');
+    }
 });
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
